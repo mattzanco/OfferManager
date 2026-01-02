75d890159d18fc7d7d1dc84f073d041d27067641
Matt Zanco <mattzanco@gmail.com>
2026-01-02 11:01:11 -0500

    install nservicebus in OfferManager.Endpoint project

--- OfferManager.Endpoint/Program.cs
+++ OfferManager.Endpoint/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using NServiceBus;
+
+class Program
+{
+	static async Task Main(string[] args)
+	{
+		var endpointConfiguration = new EndpointConfiguration("OfferManager.Endpoint");
+
+		// Use learning transport for development
+		endpointConfiguration.UseTransport<LearningTransport>();
+
+		// Optional: Use learning persistence for development
+		endpointConfiguration.UsePersistence<LearningPersistence>();
+
+		// Start the endpoint
+		var endpointInstance = await Endpoint.Start(endpointConfiguration)
+			.ConfigureAwait(false);
+
+		Console.WriteLine("NServiceBus endpoint started. Press Enter to exit.");
+		Console.ReadLine();
+
+		// Stop the endpoint
+		await endpointInstance.Stop()
+			.ConfigureAwait(false);
+	}
+}
