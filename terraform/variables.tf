9e60634c1e3fd6cabbf7532225487fb1a944d7c9
Matt Zanco <mattzanco@gmail.com>
2026-01-02 16:16:06 -0500

    configure terraform for iac

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+# Define your Terraform variables here
+
+variable "resource_group_name" {
+  description = "Azure resource group name"
+  type        = string
+  default     = "OfferManagerResources"
+}
+
+variable "location" {
+  description = "Azure region to deploy resources"
+  type        = string
+  default     = "westeurope"
+}
