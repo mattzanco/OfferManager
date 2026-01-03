a768b9b6b5bbb9a0fb60460fd2ab603e4b11aca9
Matt Zanco <mattzanco@gmail.com>
2026-01-03 09:56:16 -0500

    Add Azure Key Vault resource to Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,17 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# Azure Key Vault for the app
+resource "azurerm_key_vault" "app" {
+  name                        = "${var.resource_group_name}-kv"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+}
+
+data "azurerm_client_config" "current" {}
