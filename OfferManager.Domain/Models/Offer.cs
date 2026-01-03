03b2b6a473543ca9e27581e428d8728f42da9713
Matt Zanco <mattzanco@gmail.com>
2026-01-03 09:08:45 -0500

    Create basic User domain model.  Add additonal props to Offer model.

--- OfferManager.Domain/Models/Offer.cs
+++ OfferManager.Domain/Models/Offer.cs
@@ -3,6 +3,13 @@ namespace OfferManager.Domain.Models;
 public class Offer
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public DateTime CreateDateUtc { get; set; }
+    public DateTime UpdateDateUtc { get; set; }
+    public DateTime StartDateUtc { get; set; }
+    public DateTime ExpirationDateUtc { get; set; }
+    public int CreateByUserId { get; set; }
+    public int UpdateByUserId { get; set; }
 }
