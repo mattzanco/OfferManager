551257591795875ceef1bdfebbbd2ec59b88eb59
Matt Zanco <mattzanco@gmail.com>
2026-01-04 18:02:31 -0500

    configure tfstate backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "HireMattResources"
+    storage_account_name = "hirematttfstatestorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
