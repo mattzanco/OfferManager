a9864ee554e88ed8a578beb5e48984472cd0358e
Matt Zanco <mattzanco@gmail.com>
2026-01-04 13:39:42 -0500

    Add validation to app_name and env variables to ensure only valid characters are allowed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,12 +3,20 @@ variable "env" {
   default     = "dev"
   description = "Environment name for resource naming (e.g., dev, prd)"
   type        = string
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.()-]+$", var.env))
+    error_message = "The env variable may only contain alphanumeric characters, dashes, underscores, parentheses, and periods."
+  }
 }
 # Application name for resource naming
 variable "app_name" {
   description = "Application name for resource naming"
   type        = string
   default     = "offermanager"
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.()-]+$", var.app_name))
+    error_message = "The app_name variable may only contain alphanumeric characters, dashes, underscores, parentheses, and periods."
+  }
 }
 # Define your Terraform variables here
 
