37e7f71f0990da91fa72ac73910c77613452e97f
Matt Zanco <mattzanco@gmail.com>
2026-01-05 17:08:55 -0500

    Add environment-based Azure Key Vault integration for DB connection string

--- OfferManager.WebApi/Program.cs
+++ OfferManager.WebApi/Program.cs
@@ -1,10 +1,25 @@
 
+using Azure.Identity;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Determine Key Vault name based on environment
+string keyVaultName = builder.Environment.IsDevelopment()
+    ? "offermanager-dev-kv"
+    : "offermanager-prd-kv";
+
+string keyVaultUri = $"https://{keyVaultName}.vault.azure.net/";
+
+// Use modern AddAzureKeyVault overload from Azure.Extensions.AspNetCore.Configuration.Secrets
+builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+
+// Example: Read DB connection string from Key Vault
+string dbConnectionString = builder.Configuration["DbConnectionString"];
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 var app = builder.Build();
 
