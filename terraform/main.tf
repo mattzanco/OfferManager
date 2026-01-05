d6bf2ec0b3d8d0686ad02a4816920991817302dc
Matt Zanco <mattzanco@gmail.com>
2026-01-05 12:27:29 -0500

    Remove variable interpolation from backend key and set key via backend-config in pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "HireMattResources"
     storage_account_name = "hirematttfstatestorage"
     container_name       = "tfstate"
-    key                  = "offermanager.tfstate-${var.env}"
+      key                  = "offermanager.tfstate"
   }
   required_providers {
     azurerm = {
