a67f7893d779458e01d01225960f41e03474cf2f
Matt Zanco <mattzanco@gmail.com>
2026-01-06 12:20:08 -0500

    Add DbUp database migration project and pipeline stage
    - Create OfferManager.DbUp console application with DbUp package
    - Add sample SQL migration script for initial schema
    - Update Azure Pipelines to run migrations after Terraform
    - New DatabaseMigrations stage executes DbUp after infrastructure setup

---
+++ OfferManager.DbUp/Program.cs
@@ -0,0 +1,25 @@
+﻿using DbUp;
+
+var connectionString = Environment.GetEnvironmentVariable("DbConnectionString") 
+    ?? "Server=localhost;Database=OfferManagerDb;Integrated Security=true;";
+
+var upgrader = DeployChanges.To
+    .SqlDatabase(connectionString)
+    .WithScriptsFromFileSystem("./Scripts")
+    .WithTransaction()
+    .LogToConsole()
+    .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+    Environment.Exit(1);
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
