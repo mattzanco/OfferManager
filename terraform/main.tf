a553a8c7cce6b6403561e5052bb85b4ceaf391fe
Matt Zanco <mattzanco@gmail.com>
2026-01-10 08:55:32 -0500

    Update AKS node pool VM size to Standard_D2s_v3 for region compatibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_D2s_v3"
   }
 
   identity {
