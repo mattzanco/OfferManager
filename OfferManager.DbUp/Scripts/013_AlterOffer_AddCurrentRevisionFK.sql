d583517d44e1bd34d6e65082a35d04c345f38625
Matt Zanco <mattzanco@gmail.com>
2026-01-20 11:31:34 -0500

    Fix: Add constraint existence check to 013_AlterOffer and IF NOT EXISTS to 017_CreateLoadMilestone

--- OfferManager.DbUp/Scripts/013_AlterOffer_AddCurrentRevisionFK.sql
+++ OfferManager.DbUp/Scripts/013_AlterOffer_AddCurrentRevisionFK.sql
@@ -1,3 +1,11 @@
-ALTER TABLE offermanager.Offer
-ADD CONSTRAINT FK_Offer_CurrentRevision
-FOREIGN KEY (CurrentRevisionId) REFERENCES offermanager.OfferRevision(OfferRevisionId);
+IF NOT EXISTS (SELECT * FROM sys.key_constraints WHERE name = 'FK_Offer_CurrentRevision' AND parent_object_id = OBJECT_ID('offermanager.Offer'))
+BEGIN
+    ALTER TABLE offermanager.Offer
+    ADD CONSTRAINT FK_Offer_CurrentRevision
+    FOREIGN KEY (CurrentRevisionId) REFERENCES offermanager.OfferRevision(OfferRevisionId);
+    PRINT 'Added foreign key: FK_Offer_CurrentRevision'
+END
+ELSE
+BEGIN
+    PRINT 'Foreign key FK_Offer_CurrentRevision already exists'
+END
